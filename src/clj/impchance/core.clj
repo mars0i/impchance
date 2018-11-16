bc8798485e13795ff6281d3ffa6aaf358875e581
mars0i <mabrams@uab.edu>
2018-11-16 00:09:07 -0600

    added apply-concat and make-outcomes

--- src/clj/impchance/core.clj
+++ src/clj/impchance/core.clj
@@ -25,11 +25,23 @@
   [rng top ps]
   (let [len (choose-length rng top)
         p (choose-p rng ps)]
-    (println len p) ; DEBUg
+    ;(println len p) ; DEBUg
     (repeatedly len (fn [] (bernoulli rng p)))))
+
+;; Returns a lazy sequence concatenated from the sequences in the
+;; sequence of sequences xss (which can be lazily-generated).
+;; by OlegTheCat from https://stackoverflow.com/a/41618221/1455243
+(defn apply-concat [xss]
+  (lazy-seq
+   (when-let [s (seq xss)]
+     (concat (first s) (apply-concat (rest s))))))
+
+(defn make-outcomes
+  [rng top ps]
+  (apply-concat (repeatedly (fn [] (make-n-outcomes rng top ps)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
     (println "Hello, World!\n")
-    (println (make-n-outcomes my-rng my-top my-ps)))
+    (println (take 1000 (make-outcomes my-rng my-top my-ps))))

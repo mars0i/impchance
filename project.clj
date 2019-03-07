3f43c0bf9115184678a4d7a67ef9d52606c33b82
mars0i <mabrams@uab.edu>
2019-03-07 14:37:12 -0600

    added numeric-tower and combinatoric

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/math.combinatorics "0.1.4"]]
   :main impchance.core
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]

063edbeb95ec7c92a37c5cb2548054a563665dce
mars0i <mabrams@uab.edu>
2019-03-08 11:29:29 -0600

    added incanter charts

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/math.combinatorics "0.1.4"]]
+                 ;[org.clojure/math.combinatorics "0.1.4"]
+                 [incanter/incanter "1.9.3"]]
   :main impchance.core
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]

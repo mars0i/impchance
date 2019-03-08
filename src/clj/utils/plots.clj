9df797e53b7579f232ec26f1a4288b2b1e6e226c
mars0i <mabrams@uab.edu>
2019-03-08 11:29:36 -0600

    added plot utilities, moved random* to utils

---
+++ src/clj/utils/plots.clj
@@ -0,0 +1,43 @@
+;;; This software is copyright 2015, 2019 by Marshall Abrams, and
+;;; is distributed under the Gnu General Public License version 3.0 as
+;;; specified in the file LICENSE.
+
+;; Plot beta distributions, with various parameterizations.
+;; NOTE: This is *not* used by the intermittran simulation
+;; Intermittran uses the Beta class from MersenneTwisterFast.
+;; This file, which uses Incanter's pdf-beta, is *just* a utility
+;; to make it easy to plot various beta functions and experiment to
+;; see what different parameter combinations do.
+
+;; NOTE: These plots use JFreeChart via Incanter.  When the mean is too close to 0 or 1,
+;; the chart becomes non-sensical; the y ticks disappear, and nothing is plotted.
+;; You can still make a plot e.g. with R in this situation.
+
+;; Other useful things:
+;; (use '[incanter.pdf])
+;; (save-pdf xyp filename)
+;; where xyp is a plot object.
+
+(ns utils.plots
+  (:require [incanter.core :as ic]
+            [incanter.charts :as ich]))
+
+(defn simple-replot
+  "Given plot object xyp, adds the values in ys in relation to x values from 0
+  to the length of ys, displays the modified plot object, and returns it.  If n
+  is provided, no more than the first n elements of ys will be used."
+  ([xyp ys] (let [xs (range (count ys))]
+              (ic/view xyp)
+              (ich/add-lines xyp xs ys)))
+  ([xyp n ys] (simple-replot xyp (take n ys))))
+
+(defn simple-plot
+  "Displays a plot of the values in ys in relation to x values from 0 to the length
+  of ys, and returns the plot object.  A new xy-plot object is generated and returned.
+  If n is provided, no more than the first n elements of ys will be used."
+  ([ys] (simple-replot (ich/xy-plot) ys))
+  ([n ys] (simple-replot (ich/xy-plot) n ys)))
+
+(defn mean
+  [xs]
+  (/ (reduce + xs) (count xs)))

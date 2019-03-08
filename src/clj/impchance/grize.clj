328633073d25258940e26c8f7cac9cbce361f27c
mars0i <mabrams@uab.edu>
2019-03-08 11:30:28 -0600

    added lazy seqs for means and elements of I.4.6

--- src/clj/impchance/grize.clj
+++ src/clj/impchance/grize.clj
@@ -3,6 +3,8 @@
 (ns impchance.grize
   (:use clojure.math.numeric-tower))
 
+(def pos-ints (drop 1 (range)))
+
 ;; Example I.4.6
 
 ;; Use this to find the value of 2^{2l}, then check whether the exponent is odd 
@@ -18,10 +20,20 @@
         [prev-power e]
         (recur e power)))))
 
-(defn i46
+(defn i46-mean
   "Return the average from 1 to n as specified in example I.4.6."
   [n]
   (let [[pow exponent] (lesser-power 2 n)
         m (- n pow)
         s (if (even? exponent) 2 1)]
     (float (- s (/ m pow)))))
+
+(def i46-means (map i46-mean pos-ints))
+
+(def i46-elts
+  (let [means (map i46-means pos-ints)]
+    (map (fn [n' xn' xn] (- (* (inc n') xn) (* n' xn'))) ;; let "'" say -1, so n' = n-1, xn' = x_{n-1}:
+         pos-ints
+         i46-means
+         (drop 1 i46-means))))
+
